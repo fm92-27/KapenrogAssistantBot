47384e19e23881eaca23aec075027ee302c19364
Filintzev Michael <fm92-27@mail.ru>
2024-05-28 20:00:17 +0300

    Test read excel file vol3.4.2

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 const TelegramBot = require('node-telegram-bot-api');
+const axios = require('axios');
+const xlsx = require('xlsx');
+const fs = require('fs');
 
-// Вставьте ваш токен Telegram-бота
 const token = '6417160738:AAHXA2LCdObDBtVwR65X0VQtOsIEgf8-BoM';
 const bot = new TelegramBot(token, { polling: true });
 
@@ -8,6 +10,37 @@ bot.onText(/\/start/, (msg) => {
 	const chatId = msg.chat.id;
 	bot.sendMessage(chatId, 'Привет! Я ваш Telegram-бот.');
 });
+
+bot.onText(/\/getdata/, async (msg) => {
+	const chatId = msg.chat.id;
+	const fileUrl = 'https://docs.google.com/spreadsheets/d/1wyBwAm3ZQJOFZd5JNXb0PeGDMWznMT2Y/edit?usp=sharing&ouid=104967868883938205514&rtpof=true&sd=true';
+
+	try {
+		const res = await axios.get(fileUrl, {responseType: 'arraybuffer'});
+		const buffer = Buffer.from(res.data, 'binary');
+
+		const tempFilePath = './temp.xlsx';
+		fs.writeFileSync(tempFilePath, buffer);
+
+		const workbook = xlsx.readFile(tempFilePath);
+		const sheetName = workbook.SheetNames[0];
+		const sheet = workbook.Sheets[sheetName];
+		const data = xlsx.utils.sheet_to_json(sheet, { defval: '', raw: false });
+
+		let message = 'Данные из Excel файла:\n';
+		data.forEach((row, index) => {
+			const rowData = Object.values(row).filter(value => value.trim() !== '').join(' ');
+			if (rowData.trim() !== '') {
+				message += `Строка ${index + 2}: ${rowData}\n`;
+			}
+		});
+
+		bot.sendMessage(chatId, message);
+	} catch (error) {
+		console.error('Ошибка при получении данных из файла:', error);
+		bot.sendMessage(chatId, `Произошла ошибка при получении данных из файла. ${error}`);
+	}
+})
 
 bot.on('message', (msg) => {
 	const chatId = msg.chat.id;
