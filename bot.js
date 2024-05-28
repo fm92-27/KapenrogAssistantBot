8d0f2795b4c9eb0b2df1b80e24ebba6ee3046f1c
Filintzev Michael <fm92-27@mail.ru>
2024-05-29 01:43:24 +0300

    Test read excel file vol3.4.11.28

--- bot.js
+++ bot.js
@@ -26,8 +26,9 @@ bot.onText(/\/getdata/, async (msg) => {
 		const sheetName = workbook.SheetNames[0];
 		const sheet = workbook.Sheets[sheetName];
 		const data = xlsx.utils.sheet_to_json(sheet, { defval: '', raw: false});
-		console.log(data);
-		console.log(typeof(data));
+		if (data.includes('__EMPTY')) {
+			console.log(data);
+		};
 
 		let message = 'Данные из Excel файла:\n';
 		const ignoreIndex = [];
