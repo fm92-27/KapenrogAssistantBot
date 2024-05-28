a2db7ab6a6cda71a33eb4073c9d76af6d3616677
Filintzev Michael <fm92-27@mail.ru>
2024-05-28 17:00:26 +0300

    Test read excel file vol3.17

--- bot.js
+++ bot.js
@@ -34,6 +34,7 @@ bot.onText(/\/getdata/, async (msg) => {
         data.slice(1).forEach((row, index) => {
           // Предполагаем, что первый столбец содержит флажки
           const flag = row[Object.keys(row)[0]]; // Значение флажка в первом столбце
+          console.log(flag);
         
           if (flag === 1) { // Проверяем, включен ли флажок
             const rowData = Object.keys(row)
