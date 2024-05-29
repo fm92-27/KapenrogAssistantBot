82ab7d351b49481520bc63bee2151b047bccc7d1
Filintzev Michael <fm92-27@mail.ru>
2024-05-29 16:15:55 +0300

    Test create button vol1

--- bot.js
+++ bot.js
@@ -49,16 +49,27 @@ bot.onText(/\/getdata/, async (msg) => {
 				});
 
 			if (!ignoreIndex.includes(index)) {
-				message += `Поставщик: ${rowData[0]}\n`;
+				//message += `Поставщик: ${rowData[0]}\n`;
 
-				botReply.command('Выберите данные:', async (ctx) => {
+				const returnData = () => {
+					botReply.command('Выберите данные:', async (ctx) => {
+						const buttons = createButtons(rowData[0]);
+						ctx.reply(Markup.inlineKeyboard(buttons));
+					});
+					botReply.action(/\data_\d+/, (ctx) => {
+						const indexReply = parseInt(ctx.match[0].split('_')[1]);
+					});
+				};
+				message += `Поставщик: ${returnData}\n`;
+
+				/*botReply.command('Выберите данные:', async (ctx) => {
 					const buttons = createButtons(rowData[0]);
 					ctx.reply(Markup.inlineKeyboard(buttons));
 				});
 				botReply.action(/\data_\d+/, (ctx) => {
 					const indexReply = parseInt(ctx.match[0].split('_')[1]);
 				});
-				botReply.launch();
+				botReply.launch();*/
 			}
 		});
 
