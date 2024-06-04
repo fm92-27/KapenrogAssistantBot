e9d95f658d4a988008b9a6bfa506270fee6cb9cc
Filintzev Michael <fm92-27@mail.ru>
2024-06-04 21:56:46 +0300

    new bot build v2

--- dist/hello.js
+++ dist/hello.js
@@ -1,5 +1,5 @@
 module.exports = (bot, msg, chatId) => {
     if (msg.text.toLowerCase() === '/start') {
-        bot.sendMessage(chatId, 'Привет! Я ваш Telegram-бот./nЯ мало что могу, но я учусь!');
+        bot.sendMessage(chatId, 'Привет! Я ваш Telegram-бот.\nЯ мало что могу, но я учусь!');
     }
 };
