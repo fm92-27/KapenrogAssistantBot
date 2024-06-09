667cf802b8eb7ba762af8a598167b151c2569c38
Filintzev Michael <fm92-27@mail.ru>
2024-06-09 21:34:42 +0300

    start v0.2.3

--- dependencies.js
+++ dependencies.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+//require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const TelegramBot = require('node-telegram-bot-api');
