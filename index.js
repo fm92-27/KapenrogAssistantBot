6e7d57728d91bc42179b454c5786b6837cab4ff7
Filintzev Michael <fm92-27@mail.ru>
2024-06-16 21:41:22 +0300

    new bot 1.11

--- index.js
+++ index.js
@@ -7,14 +7,16 @@ const TOKEN = process.env.TOKEN;
 const app = express();
 app.use(bodyParser.json());
 
-const bot = new TelegramBot(TOKEN);
+require('./bot.js');
+
+/*const bot = new TelegramBot(TOKEN);
 
 bot.setWebHook(`https://${process.env.VERCEL_URL}/bot${TOKEN}`);
 
 app.post(`/bot${TOKEN}`, (req, res) => {
   bot.processUpdate(req.body);
   res.sendStatus(200);
-});
+});*/
 
 app.get('/', (req, res) => {
   res.send('Bot is running');
